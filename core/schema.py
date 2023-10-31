78ccf175afa18638d16528a07608a2907468f54c
Marianne <marianne.garridom@gmail.com>
2023-10-30 21:17:30 -0600

    aplización terminada

--- core/schema.py
+++ core/schema.py
@@ -1,6 +1,68 @@
 import graphene
 
-class Query(graphene.ObjectType):
-    hello: graphene.String(default_value= "Hi!")
+from graphene_django import DjangoObjectType
+from book.models import Book
+
+
+class BookType(DjangoObjectType):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "description", "created_at",
+                  "upated_at")
+class CreateBookMutation(graphene.Mutation):
+    class Arguments:
+        title = graphene.String()
+        description = graphene.String()
+        
+    book = graphene.Field(BookType)
     
-schema = graphene.Schema(query=Query)
+    def mutate(self, info, title, description):
+        book = Book(title = title, description = description)
+        book.save()
+        return CreateBookMutation(book=book)
+    
+
+class DeleteBookMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required = True)
+    message = graphene.String()
+    
+    def mutate(self, info, id):
+        book = Book.objects.get(pk=id)
+        book.delete()
+        return DeleteBookMutation(message = "Book Deleted") 
+    
+class UpdateBookMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required = True)
+        title = graphene.String()
+        description = graphene.String()
+        
+    book = graphene.Field(BookType)   
+    
+    def mutate(self, info, id, title, description):
+        book = Book.objects.get(pk = id)
+        book.title = title
+        book.description = description
+        book.save()
+        return UpdateBookMutation(book = book)
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hello!")
+    books = graphene.List(BookType)
+    book = graphene.Field(BookType, id=graphene.ID())
+
+    def resolve_books(self, info):
+        return Book.objects.all()
+
+    def resolve_book(self, info, id):
+        return Book.objects.get(pk=id)
+
+
+class Mutation(graphene.ObjectType):
+    create_book = CreateBookMutation.Field()
+    delete_book = DeleteBookMutation.Field()
+    update_book = UpdateBookMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'book',
-    'graphene-django'
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
